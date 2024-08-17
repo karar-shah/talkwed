cb77f5ccab2cc7a4a3814a165dc8b9adcd8c504b
Sajid Munawar <iamsajiddev@gmail.com>
2024-08-17 15:05:54 +0500

    Added my profile page

---
+++ src/components/MyProfile/Visits/Visits.tsx
@@ -0,0 +1,17 @@
+const Visits = () => {
+  return (
+    <div className="py-10 px-7">
+      <h3 className="text-lg font-semibold text-[#353535] ">
+        Viewed my profile
+      </h3>
+      <div className="border px-5 pt-3 pb-4 text-center max-w-36 mt-6">
+              <img src='/friends/f1.svg' alt='Cliodhna' />
+              <div className="pt-4 font-semibold text-[#353535]">
+              Cliodhna
+              </div>
+            </div>
+    </div>
+  );
+};
+
+export default Visits;
