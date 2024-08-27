d5d7f79399e9d0ebe0aab376673e6fc83cc374eb
Sajid Munawar <iamsajiddev@gmail.com>
2024-08-28 00:40:22 +0500

    wip

--- src/app/(clientapp)/client/vendor-manager/page.tsx
+++ src/app/(clientapp)/client/vendor-manager/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { Button, Progress } from "@nextui-org/react";
+import { Button, Progress, Select, SelectItem } from "@nextui-org/react";
 import { useState } from "react";
 import { BiPlus } from "react-icons/bi";
 import { BsArrowRight } from "react-icons/bs";
-import { FaRegHeart } from "react-icons/fa";
+import { FaRegEdit, FaRegHeart } from "react-icons/fa";
+import { FiTrash2 } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
 import { IoMdClose } from "react-icons/io";
+import { IoStar } from "react-icons/io5";
 
 const category = [
   {
@@ -35,17 +38,19 @@ const category = [
 
 const Page = () => {
   const [selected, setSelected] = useState<"favourite" | "hired">("favourite");
-  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([
+    "Venue",
+    "Catering",
+  ]);
 
   const addCategory = (title: string) => {
     if (!selectedCategory.includes(title)) {
-      setSelectedCategory(prev => [...prev, title]);
+      setSelectedCategory((prev) => [...prev, title]);
     }
   };
 
-
   const handleClearFilter = (title: string) => {
-    setSelectedCategory(prev => prev.filter(c => c !== title));
+    setSelectedCategory((prev) => prev.filter((c) => c !== title));
   };
 
   return (
@@ -68,7 +73,9 @@ const Page = () => {
 
               {selectedCategory.length && (
                 <div className="space-y-2">
-                  <p className="text-[#595959] font-semibold mt-8">Applied filters</p>
+                  <p className="text-[#595959] font-semibold mt-8">
+                    Applied filters
+                  </p>
                   {selectedCategory.map((item, i) => (
                     <Button
                       variant="faded"
@@ -100,7 +107,13 @@ const Page = () => {
                     key={i}
                     onClick={() => addCategory(item.title)}
                   >
-                    <div className={`${selectedCategory.includes(item.title) ? "text-[#767676] font-bold" : "font-semibold text-[#808080]"} `}>
+                    <div
+                      className={`${
+                        selectedCategory.includes(item.title)
+                          ? "text-[#767676] font-bold"
+                          : "font-semibold text-[#808080]"
+                      } `}
+                    >
                       {item.title}{" "}
                       <span className="font-medium">({item.count})</span>
                     </div>
@@ -150,7 +163,9 @@ const Page = () => {
               </Button>
             </div>
             <>
-              {selected === "favourite" ? (
+              {selectedCategory.length ? (
+                <FilteredVendors />
+              ) : selected === "favourite" ? (
                 <FavouriteVendors />
               ) : (
                 <HiredVendors />
@@ -190,11 +205,12 @@ const favouritesContent = [
     buttonText: "Photography",
   },
   {
-    bgUrl: "/form1.png",
+    bgUrl: "/venue_bg.svg",
     heartCount: 1,
     buttonText: "Venue",
   },
 ];
+
 const FavouriteVendors = () => (
   <div className="border rounded-lg flex-grow">
     <div className="flex flex-wrap p-4 -mx-2">
@@ -235,6 +251,79 @@ const FavouriteVendors = () => (
           </div>
         </div>
       ))}
+    </div>
+  </div>
+);
+export const categoryTypes = [
+  { key: "DressAttire", label: "Dress & Attire" },
+  { key: "Guests", label: "Guests" },
+  { key: "Planning", label: "Planning" },
+  { key: "Vendors", label: "Vendors" },
+  { key: "Invitations", label: "Invitations" },
+  { key: "Jewelry", label: "Jewelry" },
+  { key: "Legal", label: "Legal" },
+];
+const FilteredVendors = () => (
+  <div className="border rounded-lg flex-grow">
+    <p className="font-semibold text-[#808080] p-5 mb-2">
+      Still looking for vendor?{" "}
+      <span className="underline text-brand">Search vendor</span>
+    </p>
+    <div className="flex flex-wrap p-4 -mx-2">
+      <div className="px-2 w-4/12 ">
+        <div className="border  rounded-md overflow-hidden">
+          <div
+            className={`min-h-60 relative bg-[url('/venue_bg.svg')] bg-no-repeat bg-center bg-cover overflow-hidden`}
+          >
+            <div className="flex items-center justify-center absolute top-3 right-3 p-2 cursor-pointer z-10 bg-white/30">
+              <FiTrash2 color="white" size={18} />
+            </div>
+
+            <div className="size-full bg-black bg-opacity-30 absolute top-0 left-0 flex items-center justify-end flex-col pb-4 px-4">
+              <div className="bg-white/70 text-[#333333] w-full rounded-md py-2 px-3">
+                <div className="line-clamp-1 text-lg font-bold">
+                  Blue Venado Beach...
+                </div>
+                <div className="font-medium">Hastings, MI</div>
+                <div className="flex items-center gap-1">
+                  {" "}
+                  <IoStar color="#EAAE00" size={18} />
+                  <span className="font-semibold">4.9</span>
+                  <span className="font-medium">(145)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2.5 px-3.5 space-y-5">
+            <Select
+              items={categoryTypes}
+              placeholder="Select category"
+              className=""
+              variant="faded"
+              radius="sm"
+              classNames={{
+                trigger: "triggerh-fit border-none bg-[#FFF5DA]",
+                value: "value text-[#333333] font-medium",
+              }}
+            >
+              {(categoryType) => (
+                <SelectItem key={categoryType.key}>
+                  {categoryType.label}
+                </SelectItem>
+              )}
+            </Select>
+            <div className="flex gap-3">
+              <FaRegEdit color="#838383" size={20} />
+              <div className="underline text-brand">Add note</div>
+            </div>
+          </div>
+          <div className="bg-[#F8F8F8] text-[#333333] flex items-center px-3.5 py-3 space-x-3 mt-10">
+            <HiOutlineMail color="#7A7A7A" size={22} />
+            <span className="font-medium">Contact </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
