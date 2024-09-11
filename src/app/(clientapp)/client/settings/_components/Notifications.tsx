d9e14c7212d6a195d5d7fee35a9424b3b24fd675
Sajid Munawar <iamsajiddev@gmail.com>
2024-09-12 01:10:53 +0500

    Settings swtich and navigation

--- src/app/(clientapp)/client/settings/_components/Notifications.tsx
+++ src/app/(clientapp)/client/settings/_components/Notifications.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { Switch } from "@nextui-org/react";
-import Image from "next/image";
-import React from "react";
-
+import SwitchContainer from "./SwitchContainer";
+const emailNotificatoinsTitles=["Sends you a message","Participates in one of your discussions",'Requests to add you as a friend','Accepts your friend request','Quotes one of your post']
+const newslettersTitles=["TalkWed’s newsletter",'Exclusive deals and discounts only available to TalkWed members']
 const Notifications = () => {
-  const [isOn, setIsOn] = React.useState(true);
   return (
     <div className="space-y-9">
       <h2 className="font-bold text-[#444444] text-2xl">Notifications</h2>
@@ -16,208 +14,33 @@ const Notifications = () => {
           <p className="font-semibold  text-medium mb-6">
             Receive an email when someone
           </p>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium">Sends you a message</p>
-
-            <Switch
-              defaultSelected
-              size="lg"
-              color="default"
-              thumbIcon={({ isSelected, className }) =>
-                isSelected ? (
-                  <div className="text-red-700">On</div>
-                ) : (
-                  <div>OFF</div>
-                )
-              }
-            ></Switch>
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className=" font-semibold text-medium text-[#6C6C6C]">
-              Participates in one of your discussions
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Requests to add you as a friend
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Accepts your friend request
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Quotes one of your post
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
+         {
+          emailNotificatoinsTitles.map((title,index)=>(
+            <SwitchContainer key={index} title={title}/>
+          ))
+         }
+         
         </div>
         <div className="pl-12 pr-11 space-y-3 pt-9">
           <h3 className="font-semibold text-lg text-[#343434]">Newsletters</h3>
-          <div className="flex justify-between items-center">
-            <p className=" font-semibold text-medium text-[#6C6C6C]">
-              TalkWed&apos;s newsletter
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className=" font-semibold text-medium text-[#6C6C6C]">
-              Exclusive deals and discounts only available to TalkWed members
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
+          {
+            newslettersTitles.map((title,index)=>(
+              <SwitchContainer key={index} title={title}/>
+            ))
+          }
         </div>
       </div>
       <div className="w-full border border-[#DADADA] rounded-md overflow-hidden">
         <h3 className="font-semibold text-lg bg-[#F9F2FE] text-[#535353] pt-3 pb-3.5 px-12">
           App Notifications
         </h3>
-        <div className="px-12 py-8 text-[#6C6C6C] space-y-2.5">
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium">Sends you a message</p>
-            {isOn ? (
-              <Image
-                src="/On.png"
-                width={73}
-                height={36}
-                alt="On-btn"
-                onClick={() => {
-                  setIsOn(false);
-                }}
-              />
-            ) : (
-              <Image
-                src="/off.png"
-                width={73}
-                height={36}
-                alt="On-btn"
-                onClick={() => {
-                  setIsOn(true);
-                }}
-              />
-            )}
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className=" font-semibold text-medium text-[#6C6C6C]">
-              Participates in one of your discussions
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Requests to add you as a friend
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Accepts your friend request
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-          <div className="border border-[#D0D0D0]"></div>
-          <div className="flex justify-between items-center">
-            <p className="font-semibold text-medium text-[#6C6C6C]">
-              Quotes one of your post
-            </p>
-            <Image
-              src="/On.png"
-              width={73}
-              height={36}
-              alt="On-btn"
-              onClick={() => {
-                setIsOn(false);
-              }}
-            />
-          </div>
-        </div>
+        <div className="px-12 pt-5 text-[#6C6C6C] space-y-2.5">
+        {
+          emailNotificatoinsTitles.map((title,index)=>(
+            <SwitchContainer key={index} title={title}/>
+          ))
+         }
+         </div>
       </div>
     </div>
   );
--- src/app/(clientapp)/client/settings/page.tsx
+++ src/app/(clientapp)/client/settings/page.tsx
@@ -11,22 +11,51 @@ enum SETTINGS {
 }
 
 const Page = () => {
-  const [selectedSetting, setSelectedSetting] = useState<SETTINGS>(SETTINGS.NOTIFICATIONS);
+  const [selectedSetting, setSelectedSetting] = useState<SETTINGS>(
+    SETTINGS.NOTIFICATIONS
+  );
   return (
     <div className="max-w-[1280px] mx-auto px-6">
       <div className="flex flex-wrap -mx-3 pt-5 pb-20">
-        <div className="w-full md:w-3/12 px-3">
-          <h2 className="font-bold text-[#595959] text-lg py-6">Settings</h2>
-          <ul>
-            <li className="cursor-pointer"  onClick={()=>setSelectedSetting(SETTINGS.USER_INFORMATION)}>User Information</li>
-            <li className="cursor-pointer" onClick={()=>setSelectedSetting(SETTINGS.ACCOUNT_SETTINGS)}>Account Settings</li>
-            <li className="cursor-pointer" onClick={()=>setSelectedSetting(SETTINGS.NOTIFICATIONS)}>Notifications</li>
+        <nav className="w-full md:w-3/12 pl-3 pr-12">
+          <p className="font-bold text-[ #595959] text-lg">Settings</p>
+          <ul className="space-y-4 pt-10">
+            <li
+              onClick={() => setSelectedSetting(SETTINGS.USER_INFORMATION)}
+              className={`hover:cursor-pointer font-medium  text-medium text-[#6F6F6F] ${
+                selectedSetting === SETTINGS.USER_INFORMATION
+                  ? "font-semibold text-[#5A5A5A] border-l-5 border-[#9924E9] py-3 pl-[15px] pr-[18px] bg-[#F9F2FE] "
+                  : ""
+              }`}
+            >
+              User Information
+            </li>
+            <li
+              onClick={() => setSelectedSetting(SETTINGS.ACCOUNT_SETTINGS)}
+              className={`hover:cursor-pointer font-medium  text-medium text-[#6F6F6F] ${
+                selectedSetting === SETTINGS.ACCOUNT_SETTINGS
+                  ? "font-semibold text-[#5A5A5A] border-l-5 border-[#9924E9] py-3 pl-[15px] pr-[18px] bg-[#F9F2FE] "
+                  : ""
+              }`}
+            >
+              Account Settings
+            </li>
+            <li
+              onClick={() => setSelectedSetting(SETTINGS.NOTIFICATIONS)}
+              className={`hover:cursor-pointer font-medium  text-medium text-[#6F6F6F] ${
+                selectedSetting === SETTINGS.NOTIFICATIONS
+                  ? "font-semibold text-[#5A5A5A] border-l-5 border-[#9924E9] py-3 pl-[15px] pr-[18px] bg-[#F9F2FE] "
+                  : ""
+              }`}
+            >
+              Notifications
+            </li>
           </ul>
-        </div>
+        </nav>
         <div className="w-full md:w-9/12 px-3">
-        {selectedSetting === SETTINGS.USER_INFORMATION && <UserInformation />}
-        {selectedSetting === SETTINGS.ACCOUNT_SETTINGS && <AccountSettings />}
-        {selectedSetting === SETTINGS.NOTIFICATIONS && <Notifications />}
+          {selectedSetting === SETTINGS.USER_INFORMATION && <UserInformation />}
+          {selectedSetting === SETTINGS.ACCOUNT_SETTINGS && <AccountSettings />}
+          {selectedSetting === SETTINGS.NOTIFICATIONS && <Notifications />}
         </div>
       </div>
     </div>
